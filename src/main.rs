a958df27c1e85edae561a3c099359a638dd06bca
Riccardo Casatta <riccardo@casatta.it>
2023-01-31 11:06:54 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,46 @@
-
-use std::sync::mpsc::{Sender,channel};
-use std::collections::HashSet;
-use std::time::Duration;
-use std::io::Read;
-use clap::{Parser, command};
-use url::{Url, Position};
+use clap::{command, Parser};
+use reqwest::blocking::Client;
+use reqwest::header::CONTENT_TYPE;
 use select::document::Document;
 use select::predicate::Name;
-use threadpool::ThreadPool;
-use reqwest::blocking::Client;
-use reqwest::header::{CONTENT_TYPE};
+use std::collections::HashSet;
 use std::error::Error;
+use std::io::Read;
+use std::sync::mpsc::{channel, Sender};
+use std::time::Duration;
+use threadpool::ThreadPool;
+use url::{Position, Url};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-   /// Timeout for the request
-   #[arg(short, long, default_value_t = 10)]
-   timeout: u8,
+    /// Timeout for the request
+    #[arg(short, long, default_value_t = 10)]
+    timeout: u8,
 
-   /// Number of thread to use
-   #[arg(short, long, default_value_t = 4)]
-   thread: u8,
+    /// Number of thread to use
+    #[arg(short, long, default_value_t = 4)]
+    thread: u8,
 
-   /// Base url
-   #[arg(short, long)]
-   base: String
+    /// Base url
+    #[arg(short, long)]
+    base: String,
 }
 
 fn main() {
-    let args  = Args::parse();
+    let args = Args::parse();
 
     match run(args) {
         Ok(_) => println!("Finish"),
-        Err(e) => println!("{:?}",e),
+        Err(e) => println!("{:?}", e),
     }
 }
 
-fn run(args : Args) -> Result<(), Box<dyn Error>> {
-    let base =&args.base;
-    let timeout : u64 = args.timeout as u64;
-    let n_thread : usize = args.thread as usize;
+fn run(args: Args) -> Result<(), Box<dyn Error>> {
+    let base = &args.base;
+    let timeout: u64 = args.timeout as u64;
+    let n_thread: usize = args.thread as usize;
     let base = Url::parse(base)?;
     let pool = ThreadPool::new(n_thread);
     let mut url_done = HashSet::new();
@@ -51,8 +50,10 @@ fn run(args : Args) -> Result<(), Box<dyn Error>> {
     let cloned_base = base.clone();
     let cloned_tx = tx.clone();
 
-    pool.execute(move|| {
-        cloned_tx.send(cloned_base).expect("channel will be there waiting for the pool");
+    pool.execute(move || {
+        cloned_tx
+            .send(cloned_base)
+            .expect("channel will be there waiting for the pool");
     });
     let client = Client::new();
     loop {
@@ -66,36 +67,43 @@ fn run(args : Args) -> Result<(), Box<dyn Error>> {
             let cloned_tx = tx.clone();
             let cloned_base = base.clone();
             let client = client.clone();
-            pool.execute(move|| {
-
+            pool.execute(move || {
                 match get_url_and_extract(&url, &cloned_base, cloned_tx, client) {
-                    Ok(s) => println!("OK {} {}", url, s.map(|e| e.to_string()).unwrap_or_else(|| "".to_owned())),
+                    Ok(s) => println!(
+                        "OK {} {}",
+                        url,
+                        s.map(|e| e.to_string()).unwrap_or_else(|| "".to_owned())
+                    ),
                     Err(e) => println!("KO {} {}", url, e),
                 }
             });
-
         }
-
     }
     eprintln!("{} urls", url_done.len());
 
     Ok(())
 }
 
-fn normalize(url : &url::Url) -> String {
+fn normalize(url: &url::Url) -> String {
     match url.fragment() {
         Some(_) => String::from(&url[..Position::BeforeFragment]), // TODO should be -1 ???
         None => url.to_string(),
     }
 }
 
-fn get_url_and_extract(url : &url::Url, base : &url::Url, tx : Sender<url::Url>, client: Client)  -> Result<Option<usize>, Box<dyn Error>> {
+fn get_url_and_extract(
+    url: &url::Url,
+    base: &url::Url,
+    tx: Sender<url::Url>,
+    client: Client,
+) -> Result<Option<usize>, Box<dyn Error>> {
     let mut is_head = true;
 
     let response = match client.head(url.clone()).send() {
         Ok(response) => response,
-        Err(_) => { // sometimes HEAD is not supported by the server
-            is_head=false;
+        Err(_) => {
+            // sometimes HEAD is not supported by the server
+            is_head = false;
             client.get(url.clone()).send()?
         }
     };
@@ -104,20 +112,25 @@ fn get_url_and_extract(url : &url::Url, base : &url::Url, tx : Sender<url::Url>,
     if response.status().is_success() {
         let is_internal = url.host() == base.host();
         let is_html = match response.headers().get(CONTENT_TYPE) {
-            Some(content_type) => format!("{:?}", content_type ).contains("text/html"),
+            Some(content_type) => format!("{:?}", content_type).contains("text/html"),
             None => false,
         };
         if is_internal && is_html {
             let mut response = match is_head {
                 true => reqwest::blocking::get(url.clone())?,
-                false => response,  // no need to redo the get if already done
+                false => response, // no need to redo the get if already done
             };
             let mut body = String::new();
             response.read_to_string(&mut body)?;
             let body = body.as_bytes();
             html_size = Some(body.len());
 
-            let elements = vec!( ("a","href"),("script","src"),("img","src"),("link","src") );
+            let elements = vec![
+                ("a", "href"),
+                ("script", "src"),
+                ("img", "src"),
+                ("link", "src"),
+            ];
             for element in elements {
                 Document::from_read(body)?
                     .find(Name(element.0))
@@ -134,19 +147,18 @@ fn get_url_and_extract(url : &url::Url, base : &url::Url, tx : Sender<url::Url>,
     Ok(html_size)
 }
 
-
-fn validate_and_make_full_url(path_or_external : &str, base : &url::Url) -> Option<url::Url> {
+fn validate_and_make_full_url(path_or_external: &str, base: &url::Url) -> Option<url::Url> {
     let url = match Url::parse(path_or_external) {
         Ok(url) => Some(url),
         _ => match base.join(path_or_external) {
             Ok(url) => Some(url),
             _ => None,
-        }
+        },
     };
 
     match url {
         Some(url) => {
-            if url.scheme()=="https" || url.scheme()=="http" {
+            if url.scheme() == "https" || url.scheme() == "http" {
                 Some(url)
             } else {
                 None
